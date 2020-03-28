4a5c6b48d015cfe154d5793ff9151cd6ee50b7b3
ebenezum <ebenezum@o2.pl>
2020-03-28 01:01:59 +0100

    hard to say, but it works

--- src/main/scala/jiraconnector/Connection.scala
+++ src/main/scala/jiraconnector/Connection.scala
@@ -5,12 +5,15 @@ import akka.stream.ActorMaterializer
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.model.headers.BasicHttpCredentials
+import akka.http.scaladsl.unmarshalling.Unmarshal
 import scala.concurrent.Future
 import scala.util.{Failure, Success}
+import scala.concurrent.Await
+import scala.concurrent.duration._
 
 object Connection {
 
-  def newConnection(targetServer: String, user: String, pass: String) {
+  def newConnection(targetServer: String, user: String, pass: String) :String = {
     implicit val system = ActorSystem()
     implicit val materializer = ActorMaterializer()
     implicit val executionContext = system.dispatcher
@@ -26,11 +29,10 @@ object Connection {
           headers = List(authorization)
         )
       )
-
-    responseFuture
-      .onComplete {
-        case Success(res) => println(res)
-        case Failure(_)   => sys.error("something wrong")
-      }
+      
+      val result: HttpResponse = Await.result(responseFuture, Duration.Inf)
+      val fresult:Future[String] = Unmarshal(result.entity).to[String]
+      val morefinalresult:String = Await.result(fresult, Duration.Inf)
+      return morefinalresult
   }
 }
--- src/main/scala/jiraconnector/Main.scala
+++ src/main/scala/jiraconnector/Main.scala
@@ -2,11 +2,13 @@ package jiraconnector
 
 import com.typesafe.config.ConfigFactory
 
+
 object Main {
   def main(args: Array[String]) {
     val host = ConfigFactory.load().getString("jira.PRP_SERVER_URL")
     val user = ConfigFactory.load().getString("jira.JIRA_USER")
     val pass = ConfigFactory.load().getString("jira.JIRA_API_TOKEN")
-    jiraconnector.Connection.newConnection(host, user, pass)
+    val efekt = jiraconnector.Connection.newConnection(host, user, pass)
+    println(efekt)
   }
 }
